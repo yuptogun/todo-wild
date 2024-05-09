2fecf1f028d0b1b1cccd3faad934317f2aa27f4a
yuptogun <eojin1211@hanmail.net>
2024-05-09 05:14:46 +0000

    added TailwindCSS with basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-1 md:grid-cols-2 my-5">
+      <div>
+        <h1 class="text-2xl my-3">To Do Wild</h1>
+        <p>An offline PWA app exercise by <a href="htps://yuptogun.com" target="_blank" class="text-gray-600 hover:text-gray-400 underline underline-offset-4">yuptogun</a>.</p>
+      </div>
+      <div>
+        foobar
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
