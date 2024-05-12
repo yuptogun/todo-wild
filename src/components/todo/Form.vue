dad5a8bb781e3a45fed30a6f51d9f118823b71a1
yuptogun <eojin1211@hanmail.net>
2024-05-12 05:38:14 +0000

    basic form-list binding done

--- src/components/todo/Form.vue
+++ src/components/todo/Form.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { plainToInstance } from 'class-transformer'
+import { computed, ref } from 'vue'
+import Todo from '../../entities/todo'
+import { TodoStatus } from '../../enums/TodoStatus';
 
-const todo = ref('')
+const todoList = defineModel({ type: Array })
+
+const todoInput = ref('')
+const cannotAddTodo = computed(() => !todoInput.value.length)
+
 const addTodo = () => {
-  console.log(todo);
+  // 여기쯤에 indexed db 삽입 루틴 추가 필요
+  const newTodoItem = plainToInstance(Todo, {
+    todo: todoInput.value,
+    status: TodoStatus.Undone,
+  })
+  todoList.value?.unshift(newTodoItem)
+  todoInput.value = ''
 }
 </script>
 
 <template>
-  <form v-on:submit.prevent="addTodo()">
+  <form v-on:submit.prevent="addTodo">
     <div class="flex pb-3 gap-3">
-      <input type="text" v-model="todo" class="form-input w-100 rounded flex-grow" placeholder="what needs to be done?" />
-      <button type="submit" class="bg-gray-800 text-white rounded px-5 py-3">add</button>
+      <input type="text" v-model.trim="todoInput" class="form-input w-100 rounded flex-grow" placeholder="what needs to be done?" required />
+      <button type="submit" class="bg-gray-800 text-white rounded px-5 py-3 disabled:bg-gray-200 disabled:text-gray-400" :disabled="cannotAddTodo">add</button>
     </div>
   </form>
 </template>
--- src/components/todo/List.vue
+++ src/components/todo/List.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
-import { ref, computed, defineProps } from 'vue';
+import { ref, computed, defineProps } from 'vue'
+import Todo from '../../entities/todo'
+import { TodoStatus } from '../../enums/TodoStatus'
+import { plainToInstance } from 'class-transformer'
 
-const props = defineProps(['list']);
-// const list = props.list || [];
-const list = [{id: 3, todo: 'test 3', status: 'closed'}, {id: 2, todo: 'test 2', status: 'done'}, {id: 1, todo: 'test 1', status: 'open'}];
-const todos = ref(list.filter((t) => t.status !== 'closed'));
-const allDone = computed(() => todos.value.filter((todo) => todo.status === 'open').length === 0);
-const shouldGoWild = computed(() => list.length !== 0 && todos.value.length === 0);
+const inspirations = ['https://femiwiki.com/w/GO_WILD_SPEAK_LOUD_THINK_HARD', 'https://www.bible.com/ko/bible/1/EXO.4.12']
+const suggestions = [`why don't you go wild now?`, `now go wild.`, `now toward your wilderness.`]
+const getRandom = function (items: string[]) {
+  return items[Math.floor(Math.random() * items.length)]
+}
+
+const todoList: { value: Todo[]; } = defineModel()
+// console.log(todoList.value)
+
+const openTodos = computed(() => todoList.value.filter((t: Todo) => t.isOpen()))
+const allDone = computed(() => openTodos.value.length === 0)
+const needsInspiration = computed(() => todoList.value.length !== 0 && allDone.value)
 </script>
 
 <template>
   <div>
-    <ul v-if="todos.length">
-      <li v-for="todo in todos" :key="todo.id" class="border-b border-gray-200 py-3 last:border-none">
+    <p v-if="allDone" class="text-center text-lg m-5">
+      <span v-if="needsInspiration">
+        you've got nothing else to do <a :href="getRandom(inspirations)" target="_blank" class="text-gray-700 hover:text-gray-600">{{ getRandom(suggestions) }}</a>
+      </span>
+      <span v-else>you've got nothing else to do.</span>
+    </p>
+    <ul v-else>
+      <li v-for="todo in openTodos" :key="todo.id" class="border-b border-gray-200 py-3 last:border-none">
         <label class="block w-full">
           <input type="checkbox" class="form-checkbox rounded me-2" :checked="todo.status === 'done'" />
           {{ todo.todo }}
         </label>
       </li>
     </ul>
-    <p v-if="allDone" class="text-center text-lg m-5">
-      you've got nothing else to do<span v-if="shouldGoWild">; <a href="https://www.bible.com/ko/bible/1/EXO.4.12" target="_blank" class="text-gray-700 hover:text-gray-600">now go wild</a></span>.
-    </p>
   </div>
 </template>
