9834379683f397ac0d8e5ccb43b23e60528de13d
yuptogun <eojin1211@hanmail.net>
2024-05-16 09:13:05 +0000

    extra trimming

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import Todo from './components/Todo.vue';
           <div class="mt-5 lg:my-5 lg:pe-5 lg:max-w-80 xl:max-w-96">
             <a href="/">
               <img src="/icon-192.png" class="inline me-3 lg:block lg:me-0 mb-3 w-full max-w-10 md:max-w-20" alt="To Do Wild" />
-              <strong>To Do Wild</strong>
+              <strong translate="no">To Do Wild</strong>
             </a>
             <span class="hidden md:inline">: a to-do web app that (supposedly) works off the grid.</span>
           </div>
@@ -21,8 +21,10 @@ import Todo from './components/Todo.vue';
       </div>
     </div>
     <footer class="my-5 text-center text-gray-400 text-sm">
-      &copy; 2024~ by <a href="//yuptogun.com" target="_blank" class="text-gray-400 hover:text-gray-600 underline underline-offset-4">yuptogun</a> &middot; <a href="//github.com/RandomMaerks/Overused-Grotesk" target="_blank" rel="noopener noreferrer"
-          class="underline underline-offset-4 text-gray-400 hover:text-gray-600">Overused Grotesk</a> by RandomMaerks
+      &copy; 2024~ by <a href="//yuptogun.com" target="_blank" class="text-gray-400 hover:text-gray-600 underline underline-offset-4" translate="no">yuptogun</a>
+      <span class="mx-1">&middot;</span>
+      <a href="//github.com/RandomMaerks/Overused-Grotesk" target="_blank" rel="noopener noreferrer"
+          class="underline underline-offset-4 text-gray-400 hover:text-gray-600" translate="no">Overused Grotesk</a> by <span translate="no">RandomMaerks</span>
     </footer>
   </div>
 </template>
--- src/components/todo/List.vue
+++ src/components/todo/List.vue
@@ -5,9 +5,9 @@ import Item from './Item.vue';
 
 const emit = defineEmits(['deleteTodo', 'editTodo', 'changeTodoStatus'])
 
-const inspirations = ['https://femiwiki.com/w/GO_WILD_SPEAK_LOUD_THINK_HARD', 'https://www.bible.com/bible/1/EXO.4.12']
-const suggestions = [`why don't you go wild now?`, `now go wild.`, `now toward your wilderness.`]
-const introductions = [`list what you'd need in the wilderness.`, `before you go wild, you need...`, `let's make a wild checklist shall we?`]
+// const inspirations = ['https://femiwiki.com/w/GO_WILD_SPEAK_LOUD_THINK_HARD', 'https://www.bible.com/bible/1/EXO.4.12']
+// const suggestions = [`why don't you go wild now?`, `now go wild.`, `now toward your wilderness.`]
+const introductions = [`list what you'd need in the wilderness.`]
 const getRandom = function (items: string[]) {
   return items[Math.floor(Math.random() * items.length)]
 }
@@ -16,16 +16,18 @@ const todoList = defineModel()
 
 const openTodos = computed(() => todoList.value.filter((t: Todo) => t.isOpen()))
 const allDone = computed(() => openTodos.value.length === 0)
-const needsInspiration = computed(() => todoList.value.length !== 0 && allDone.value)
+// const needsInspiration = computed(() => todoList.value.length !== 0 && allDone.value) // 방금 이벤트로 인해서 openTodos가 비어버렸을 경우에만 보여줘야 함. 조금 복잡하니 나중에 구현.
 </script>
 
 <template>
   <div>
     <div v-if="allDone" class="text-center text-lg m-5 py-5">
-      <span v-if="needsInspiration">
+      <!-- <span v-if="needsInspiration">
         guess you're ready. <a :href="getRandom(inspirations)" target="_blank" class="text-gray-700 hover:text-gray-600">{{ getRandom(suggestions) }}</a>
       </span>
-      <span v-else class="py-5">{{ getRandom(introductions) }}</span>
+      <span v-else class="py-5"> -->
+        {{ getRandom(introductions) }}
+      <!-- </span> -->
     </div>
     <ul v-else>
       <li v-for="(todo, i) in openTodos" :key="todo.id" class="border-b border-gray-200 py-3 last:border-none">
