d6efd002d332054d84bf56b09da74ba6d7e474f9
Eojin Kim <eojin1211@hanmail.net>
2024-05-25 08:22:29 +0000

    toggle archive Fixes #13

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import { computed, inject, onBeforeMount, ref } from 'vue'
+import { computed, inject, onBeforeMount, onMounted, ref } from 'vue'
 import { plainToInstance } from 'class-transformer';
 import Form from './todo/Form.vue'
 import List from './todo/List.vue'
 import Todo from '../entities/todo'
-import TodoRepo from '../repositories/todoRepo';
-import Create from '../dtos/todo/create';
+import TodoRepo from '../repositories/todoRepo'
+import Create from '../dtos/todo/create'
 
-const todos: { value: Todo[] } = ref([])
-const todoList = ref(todos)
 const repo = inject<TodoRepo>('repo', new TodoRepo())
+const todos: { value: Todo[] } = ref([])
+const todoListMode = ref('open')
+const todoList = computed(() => todos.value.filter(
+  (t: Todo) => todoListMode.value === 'open' ? t.isOpen() : t.isClosed()
+))
 
 const anyDoneTodos = computed(() => !! todos.value.filter((t: Todo) => t.isDone()).length)
 
@@ -37,6 +40,9 @@ const archiveAllSelected = () => {
     }
   })
 }
+const setTodoListMode = (mode: 'open' | 'closed') => {
+  todoListMode.value = mode
+}
 
 onBeforeMount(async () => await getTodos())
 </script>
@@ -50,19 +56,24 @@ onBeforeMount(async () => await getTodos())
     </div>
     <List class="w-full"
       v-model="todoList"
+      :mode="todoListMode"
       @delete-todo="(todo: Todo) => deleteTodo(todo)"
       @edit-todo="(todo: Todo) => editTodo(todo)"></List>
-    <div class="mt-5 sticky bottom-0 bg-gradient-to-t from-white from-90% to-transparent py-3 pt-5"
-      v-if="anyDoneTodos"><!-- to be moved into buttons -->
+    <div class="mt-5 sticky bottom-0 bg-gradient-to-t from-white from-90% to-transparent py-3 pt-5">
       <div class="flex gap-3 justify-between">
         <div>
           <button type="button" class="text-sm px-2 py-1 rounded text-slate-700 bg-slate-100 hover:bg-slate-200 focus-visible:outline-offset-2 focus-visible:outline-slate-200"
+            v-if="anyDoneTodos"
             @click="archiveAllSelected">archive all selected</button>
         </div>
-        <!-- <div>
-          <button type="button" class="text-sm px-2 py-1 rounded text-slate-700 bg-slate-100 hover:bg-slate-200"
-            @click="showArchived">show archived</button>
-        </div> -->
+        <div>
+          <button v-if="todoListMode !== 'closed'"
+            type="button" class="text-sm px-2 py-1 rounded text-slate-700 bg-slate-100 hover:bg-slate-200"
+            @click="setTodoListMode('closed')">show archived</button>
+          <button v-else
+            type="button" class="text-sm px-2 py-1 rounded text-slate-700 bg-slate-100 hover:bg-slate-200"
+            @click="setTodoListMode('open')">show open</button>
+        </div>
       </div>
     </div>
   </div>
--- src/components/todo/List.vue
+++ src/components/todo/List.vue
@@ -1,37 +1,38 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, defineModel, defineProps, onUpdated } from 'vue'
 import Todo from '../../entities/todo'
 import Item from './Item.vue';
 
 const emit = defineEmits(['deleteTodo', 'editTodo'])
+const props = defineProps(['mode'])
+const todoList = defineModel()
+const todoListMode = ref(props.mode)
 
 // const inspirations = ['https://femiwiki.com/w/GO_WILD_SPEAK_LOUD_THINK_HARD', 'https://www.bible.com/bible/1/EXO.4.12']
 // const suggestions = [`why don't you go wild now?`, `now go wild.`, `now toward your wilderness.`]
-const introductions = [`list what you'd need in the wilderness.`]
+const emptyListMessage = [`list what you'd need in the wilderness.`]
 const getRandom = function (items: string[]) {
   return items[Math.floor(Math.random() * items.length)]
 }
 
-const todoList = defineModel()
-
-const openTodos = computed(() => todoList.value.filter((t: Todo) => t.isOpen()))
-const allDone = computed(() => openTodos.value.length === 0)
-// const needsInspiration = computed(() => todoList.value.length !== 0 && allDone.value) // 방금 이벤트로 인해서 openTodos가 비어버렸을 경우에만 보여줘야 함. 조금 복잡하니 나중에 구현.
+// const openTodos = computed(() => todoList.value.filter((t: Todo) => t.isOpen()))
+const listEmpty = computed(() => todoList.value.length === 0)
+// const needsInspiration = computed(() => todoList.value.length !== 0 && listEmpty.value) // 방금 이벤트로 인해서 openTodos가 비어버렸을 경우에만 보여줘야 함. 조금 복잡하니 나중에 구현.
 </script>
 
 <template>
   <div>
-    <div v-if="allDone" class="text-center text-lg m-5 py-5">
+    <div v-if="listEmpty" class="text-center text-lg m-5 py-5">
       <!-- <span v-if="needsInspiration">
         guess you're ready. <a :href="getRandom(inspirations)" target="_blank" class="text-gray-700 hover:text-gray-600">{{ getRandom(suggestions) }}</a>
       </span>
       <span v-else class="py-5"> -->
-        {{ getRandom(introductions) }}
+        {{ getRandom(emptyListMessage) }}
       <!-- </span> -->
     </div>
     <ul v-else>
-      <li v-for="(todo, i) in openTodos" :key="todo.id" class="border-b border-gray-200 py-3 last:border-none">
-        <Item v-model="openTodos[i]"
+      <li v-for="(todo, i) in todoList" :key="todo.id" class="border-b border-gray-200 py-3 last:border-none">
+        <Item v-model="todoList[i]"
           @delete-todo="$emit('deleteTodo', todo)"
           @edit-todo="$emit('editTodo', todo)"></Item>
       </li>
