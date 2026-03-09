ce2384ca867b7f09adffbfe6e2937ca5588eb661
yuptogun <eojin1211@hanmail.net>
2026-03-09 08:16:21 +0000

    trying fixing VitePWA auto reload

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      injectRegister: 'script',
+      injectRegister: 'auto',
       registerType: 'autoUpdate',
       includeAssets: ["/favicon.ico", "/icon-192.png", "/icon-512.png", "/icon-192-maskable.png", "/icon-512-maskable.png"],
       manifest: {
