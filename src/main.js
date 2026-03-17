868ee6ac359080a31d579bc54db92142c344ee05
Eojin Kim <eojin1211@hanmail.net>
2026-03-17 16:48:49 +0000

    f02a14e26 retry

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { registerSW } from 'virtual:pwa-register';
 import IndexedDB from './plugins/IndexedDB';
 import LocalStorage from './plugins/LocalStorage';
 import App from './App.vue';
@@ -8,3 +9,7 @@ const app = createApp(App);
 
 app.use(IndexedDB).use(LocalStorage);
 app.mount('#app');
+
+registerSW({
+  immediate: true
+});
